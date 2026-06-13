6f16fe18ab412f70cbf12b43a393a81aa68e9bfb
truePhilipp <philipp@mayr.software>
2026-06-13 11:08:00 +0200

    genereal layout done

--- static/app.js
+++ static/app.js
@@ -1,17 +1,19 @@
 const cities = {
-    passau: { label: "Passau", image: "images/passau.png" },
-    berlin: { label: "Berlin", image: "images/berlin.png" },
-    munich: { label: "München", image: "images/munich.png" }
+    passau: { label: "PASSAU", image: "images/passau.png" },
+    berlin: { label: "BERLIN", image: "images/berlin.png" },
+    munich: { label: "MÜNCHEN", image: "images/munich.png" }
 };
 
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const size = 1024;
+const center = size / 2;
 
 const eventTypeInput = document.getElementById("event-type-input");
 const citySelect = document.getElementById("city-select");
 const locationInput = document.getElementById("location-input");
+const personEnabled = document.getElementById("person-enabled");
 const personInput = document.getElementById("person-input");
 const dateInput = document.getElementById("date-input");
 const pointInput = document.getElementById("point-input");
@@ -33,36 +35,43 @@ function loadCityImage(cityKey) {
     });
 }
 
+function setFontSize(fontSize) {
+    ctx.font = `${fontSize}px 'IBM Plex Mono'`;
+}
+
+function setLetterSpacing(spacing) {
+    ctx.letterSpacing = `${spacing}px`;
+}
+
+function setFontColor(color) {
+    ctx.fillStyle = color;
+}
+
 function render() {
-    canvas.width = size;
-    canvas.height = size;
-
-    if (!currentImage) {
-        ctx.fillStyle = "#e5e7eb";
-        ctx.fillRect(0, 0, size, size);
-        ctx.fillStyle = "#9ca3af";
-        ctx.font = "18px 'IBM Plex Mono'";
-        ctx.textAlign = "center";
-        ctx.fillText("No image loaded", size / 2, size / 2);
-        return;
-    }
-
-    
-
     ctx.drawImage(currentImage, 0, 0, size, size);
 
-    const padding = size * 0.1;
-    const fontSize = 40;
+    let offset = center;
 
-    ctx.font = `${fontSize}px 'IBM Plex Mono'`;
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
+    setFontSize(60);
+    setLetterSpacing(0);
+    setFontColor("#ffffff");
+    ctx.fillText(eventTypeInput.value, center, offset);
 
-    ctx.fillStyle = "#ffffff";
+    setFontSize(120);
+    setLetterSpacing(30);
+    setFontColor("#ff0000");
+    offset += 100;
+    ctx.fillText(locationInput.value, center, offset);
 
-    ctx.fillText(eventTypeInput.value, size / 2, size / 2);
-    ctx.fillText(locationInput.value, size / 2, size / 2 + 40);
-    ctx.fillText(personInput.value, size / 2, size / 2 + 80);
+    setFontSize(40);
+    setLetterSpacing(0);
+    setFontColor("#ffffff");
+    offset += 110;
+
+    if (personEnabled.checked) {
+        ctx.fillText(personInput.value, center, offset);
+        offset += 60;
+    }
     
     {
         date = new Date(dateInput.value);
@@ -75,16 +84,20 @@ function render() {
             hour: '2-digit', 
             minute: '2-digit', 
         });
-        ctx.fillText(`am ${day} ab ${time} Uhr` ,size / 2, size / 2 + 120);
+        ctx.fillText(`am ${day} ab ${time} Uhr` ,center, offset);
     }
 
-    ctx.fillText(`Treffpunkt: ${pointInput.value}`, size / 2, size / 2 + 160);
+    offset += 60;
+    ctx.fillText(`Treffpunkt: ${pointInput.value}`, center, offset);
+    
     if (addressEnabled.checked) {
-        ctx.fillText(`(${addressInput.value})`, size / 2, size / 2 + 200);
+        offset += 60;
+        ctx.fillText(`(${addressInput.value})`, center, offset);
     }
 
+    offset = center + 460;
     if (newMembersEnabled.checked) {
-        ctx.fillText("Auch für Nichtmitglieder!", size / 2, size / 2 + 240);
+        ctx.fillText("Auch für Nichtmitglieder!", center, offset);
     }
 }
 
@@ -101,6 +114,7 @@ citySelect.addEventListener("change", (e) => {
 
 eventTypeInput.addEventListener("input", render);
 locationInput.addEventListener("input", render);
+personEnabled.addEventListener("input", render);
 personInput.addEventListener("input", render);
 dateInput.addEventListener("input", render);
 pointInput.addEventListener("input", render);
@@ -121,7 +135,13 @@ function current_date() {
     return now.toISOString().slice(0,16);
 }
 
-document.fonts.ready.then(() => {
+function initialise() {
+    canvas.width = size;
+    canvas.height = size;   
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle"; 
     setCity("passau");
     dateInput.value = current_date();
-});
+}
+
+document.fonts.ready.then(initialise);
